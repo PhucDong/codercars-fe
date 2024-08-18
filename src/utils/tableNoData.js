f02973497ac0c5ca09eadf7fd180bb7f38467620
Phuc <dongvuminhphuc@gmail.com>
2024-08-18 21:45:29 +0700

    Add lineHeight when no data available in table

--- src/utils/tableNoData.js
+++ src/utils/tableNoData.js
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 // Customize announcement when there's no data in the table
 export function CustomNoRowsOverlay() {
   return (
-    <Typography sx={{ color: "#70787A", fontWeight: 550 }}>
+    <Typography sx={{ color: "#70787A", fontWeight: 550, lineHeight: "100%" }}>
       No Data Available
     </Typography>
   );
